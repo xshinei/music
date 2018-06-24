77dac3a43c672c63677fe157b9efef68cbec65bc
XShinei <497449921@qq.com>
2018-06-24 22:20:31 +0800

    config min-css-extract-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -2,14 +2,16 @@ const path = require('path');
 
 const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 
 module.exports = {
     entry: {
-
+        index: './src/index.js'
     },
     output: {
-
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
     },
     module: {
         rules: [
@@ -24,16 +26,22 @@ module.exports = {
                     path.resolve(__dirname, 'src')
                 ],
                 use: [
-                    'style-loader',
-                    'css-loader'
-                ]
+                    MiniCssExtractPlugin.loader,  // replace ExtractTextPlugin.extract({..})
+                    "css-loader"
+                  ]
             }
         ]
     },
-    plugin: [
+    plugins: [
         new HtmlWebpackPlugin({
             filename: 'index.html',
             template: 'src/index.html'
-        })
+        }),
+        new MiniCssExtractPlugin({
+            // Options similar to the same options in webpackOptions.output
+            // both options are optional
+            filename: "[name].css",
+            chunkFilename: "[id].css"
+          })
     ]
 };
