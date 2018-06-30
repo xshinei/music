86273c528abf3a8edb40df720407ce026c5b7a12
XShinei <497449921@qq.com>
2018-06-30 15:05:54 +0800

    cinfig router

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 import Vue from 'vue';
-import router from './js/routes';
+import VueRouter from 'vue-router';
+import routes from './js/routes/index';
+
+Vue.use(VueRouter);
 
 import APP from './js/pages/APP.vue';
 import './css/reset.scss';
+
+const router = new VueRouter({routes});
 
 new Vue({
     router,
