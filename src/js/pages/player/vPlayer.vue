18e3bfde42ac929c4dcf7eb2a46f775549819297
zhangan <zhangan@gaodun.com>
2018-08-23 15:48:44 +0800

    control play and switch play mode

--- src/js/pages/player/vPlayer.vue
+++ src/js/pages/player/vPlayer.vue
@@ -1,11 +1,31 @@
 <template>
     <div class="wrap" ref="wrap">
-        <audio :src="songUrl" 
+        <div class="blur-background">
+            <img :src="current_song.al.picUrl" width="100%" height="100%" alt="">
+        </div>
+        <audio :src="song_url" 
                 autoplay 
                 ref="audio" 
                 @canplay="audioCanplay" 
                 @timeupdate="audioTimeupdate">
         </audio>
+        <div class="song-message-container">
+            <img  class="bg-image" :src="current_song.al.picUrl" alt="">
+        </div>
+        <div class="option-container">
+            <div class="love">
+                <i class="fa fa-heart-o" aria-hidden="true"></i>
+            </div>
+            <div class="download">
+                <i class="fa fa-download" aria-hidden="true"></i>
+            </div>
+            <div class="comment">
+                <i class="fa fa-commenting-o" aria-hidden="true"></i>
+            </div>
+            <div class="more">
+                <i class="fa fa-ellipsis-v" aria-hidden="true"></i>
+            </div>
+        </div>
         <div class="progress-container">
             <span class="current-time">{{ currentTime }}</span>
             <div class="progress-bar" ref="progressBar" @click="handleClickProgressBar">
@@ -17,26 +37,65 @@
             </div>
             <span class="total-time">{{ duration }}</span>
         </div>
+        <div class="tool-container">
+            <div class="play-mode" @click="handleSwitchMode">
+                <i class="icon fa fa-random" aria-hidden="true"></i>
+            </div>
+            <div class="backward" @click="handleSwitchSong(-1)">
+                <i class="icon fa fa-step-backward" aria-hidden="true"></i>
+            </div>
+            <div class="play-pause" @click="handlePlay">
+                <i v-if="togglePlay" class="icon fa fa-pause" aria-hidden="true"></i>
+                <i v-else class="icon fa fa-play" aria-hidden="true"></i>
+            </div>
+            <div class="forward" @click="handleSwitchSong(1)">
+                <i class="icon fa fa-step-forward" aria-hidden="true"></i>
+            </div>
+            <div class="play-list">
+                <i class="icon fa fa-align-justify" aria-hidden="true"></i>
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
-    import { getSongUrl } from '../../api/player.js';
+    import { mapState, mapMutations } from 'vuex';
+    import { getSongUrl, getSongDetail } from '../../api/player.js';
 
     export default {
         data() {
             return {
                 duration: '',   // 歌曲的总时长
-                currentTime: '' // 歌曲播放的当前时间
+                currentTime: '00:00', // 歌曲播放的当前时间
+                togglePlay: true,  // true: play   false: pause
+                play_list_cache: []
             };
         },
         computed: {
-            
+            current_song() {
+                return this.play_list[this.current_index];
+            },
+            ...mapState([
+                'play_list',
+                'current_index',
+                'play_mode'
+            ]),
+            song_url() {
+                const URL_TPL = 'http://music.163.com/song/media/outer/url?id=id.mp3';
+
+                return URL_TPL.replace(/id(?=\.mp3)/, this.current_song.id);
+            },
         },
         created() {
-            // 由于直接访问歌曲详情链接会403
-            const urlTpl = 'http://music.163.com/song/media/outer/url?id=id.mp3';
-            this.songUrl = urlTpl.replace(/id(?=\.mp3)/, this.$route.params.id);
+            // const params = {
+            //     ids: this.$route.params.id
+            // };
+            // getSongDetail(params).then(res => {
+            //     if (res.code === 200) {
+                    
+            //     }
+            // });
+            this.play_list_cache = this.play_list;
         },
         mounted() {
             this.wrap = this.$refs.wrap;
@@ -44,28 +103,74 @@
             this.progressBar = this.$refs.progressBar;
             this.progressHasPlayed = this.$refs.progressHasPlayed;
             this.progressBtn = this.$refs.progressBtn;
-
             /**
              *  wrap touchmove事件
              */
             this.wrap.addEventListener('touchmove', (event) => {
-                const barMarginLeft = parseInt(this.getCSS(this.progressBar, 'marginLeft'));
-                const barOffsetleft = this.progressBar.offsetLeft;
-                const offset = event.touches[0].clientX - barOffsetleft;
+                if (this.start) {
+                    const barMarginLeft = parseInt(this.getCSS(this.progressBar, 'marginLeft'));
+                    const barOffsetleft = this.progressBar.offsetLeft;
+                    const offset = event.touches[0].clientX - barOffsetleft;
 
-                this.moveProgressBtn(offset);
-                this.moveProgressHasPlayed(offset);
+                    this.moveProgressBtn(offset);
+                    this.moveProgressHasPlayed(offset);
+                }
             });
 
             /**
              *  wrap touchend事件
              */
             this.wrap.addEventListener('touchend', (event) => {
-                this.setAudioTime();
-                this.audio.play();
+                if (this.start) {
+                    this.start = false;
+                    this.setAudioTime();
+                    this.audio.play();
+                }
             });
         },
         methods: {
+            handlePlay() {
+                this.togglePlay = !this.togglePlay;
+
+                if (this.togglePlay) {
+                    this.audio.play();
+                }
+                else {
+                    this.audio.pause();
+                }
+            },
+            /**
+             *  click event
+             *  type = 1: next song  type = -1: prev song
+             */
+            handleSwitchSong(type) {
+                let index = this.current_index;
+                const len = this.play_list.length;
+
+                if (type === 1) {
+                    index === len - 1 ? index = 0 : index++;
+                }
+                else if (type === -1) {
+                    index === 0 ? index = len - 1 : index--;
+                }
+
+                this.setCurrentIndex(index);
+            },
+            /**
+             *  click event
+             *  0: sequence    1: single    2: random
+             */
+            handleSwitchMode() {
+                const mode_list = [0, 1, 2];
+                const len = mode_list.length;
+                let mode = this.play_mode + 1;
+
+                mode = mode_list[mode % len];
+                
+                mode === 2 && this.randomPlayList();
+
+                this.setPlayMode(mode);
+            },
             /**
              *  progress-bar点击事件
              *  调整歌曲进度到点击位置
@@ -84,6 +189,7 @@
              *  暂停歌曲
              */
             handleProgressBtnStart(event) {
+                this.start = true;
                 this.audio.pause();
             },
             /**
@@ -100,7 +206,7 @@
              */
             audioTimeupdate() {
                 this.getCurrentTime();
-
+                
                 const percent = this.audio.currentTime / this.audio.duration;
                 this.progressHasPlayed.style.width = percent * 100 + '%';
 
@@ -167,6 +273,41 @@
              */
             getCSS(ele, attr) {
                 return window.getComputedStyle(ele)[attr];
+            },
+            /**
+             *  if calling first or component refresh or play_list change
+             */
+            randomPlayList() {
+                const list = [];
+
+                if (!this.flag) {
+                    const play_list_len = this.play_list_cache.length;
+                    const sequence_list = [];
+
+                    for (let i = 0; i < play_list_len; i++) {
+                        sequence_list.push(i);
+                    }
+
+                    while (sequence_list.length) {
+                        const index = Math.floor( Math.random() * sequence_list.length );
+
+                        list.push(this.play_list_cache[index]);
+                        sequence_list.splice(index, 1);
+                    }
+                }
+
+                this.play_list_cache = list;
+                this.flag = true;
+            },
+            ...mapMutations({
+                setPlayList: 'SET_PLAY_LIST',
+                setCurrentIndex: 'SET_CURRENT_INDEX',
+                setPlayMode: 'SET_PLAY_MODE'
+            })
+        },
+        watch: {
+            play_list(val) {
+                this.play_list_cache = val;
             }
         }
     }
@@ -175,25 +316,60 @@
 <style lang="scss" scoped>
     @import '../../../css/function.scss';
 
+    @keyframes rotate {
+        from {
+            transform: rotate(0deg);
+        }
+        to {
+            transform: rotate(360deg);
+        }
+    }
+
     .wrap {
         position: relative;
         height: 100%;
 
-        &::before {
+        .blur-background {
             content: '';
             position: absolute;
+            z-index: -1;
             top: 0;
             bottom: 0;
             left: 0;
             right: 0;
-            filter: blur(100px);
-            background-color: rgb(163, 163, 163);
+            filter: blur(20px);
+            background-color: #000;
+        }
+
+        .song-message-container {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+
+            .bg-image {
+                display: block;
+                width: setRem(608);
+                height: setRem(608);
+                border-radius: 50%;
+                animation: rotate 30s linear infinite;
+                animation-fill-mode:both;
+            }
+        }
+
+        .option-container {
+            display: flex;
+            justify-content: space-between;
+            align-items: center;
+            padding: 0 setRem(118);
+            font-size: setRem(37);
+            color: rgb(217, 217, 218);
         }
 
         .progress-container {
             display: flex;
             justify-content: center;
             align-items: center;
+            margin: setRem(20) 0;
 
             .progress-bar {
                 position: relative;
@@ -229,6 +405,47 @@
                 width: setRem(64);
                 font-size: setRem(16);
                 // color: rgb(230, 230, 230);
+            }
+        }
+
+        .song-message-container {
+            height: setRem(878);
+        }
+
+        .tool-container {
+            display: flex;
+            justify-content: space-around;
+            align-items: center;
+            height: setRem(170);
+
+            .play-mode, .play-list {
+                font-size: setRem(42);
+                color: rgb(231, 231, 231);
+            }
+
+            .backward, .forward {
+                font-size: setRem(48);
+                color: rgb(231, 231, 231);
+            }
+
+            .play-pause {
+                display: flex;
+                justify-content: center;
+                align-items: center;
+                width: setRem(92);
+                height: setRem(92);
+                border: 1px solid #fff;
+                border-radius: 50%;
+                color: #fff;
+
+                .icon {
+                    font-size: setRem(39);
+                    font-weight: lighter;
+
+                    &.fa-play {
+                        margin-left: setRem(12);
+                    }
+                }
             }
         }
     }
