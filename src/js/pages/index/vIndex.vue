cb063d1a3ff1e5e7dfa0630d94feaa04d66975af
zhangan <zhangan@gaodun.com>
2018-10-22 22:39:21 +0800

    主页布局

---
+++ src/js/pages/index/vIndex.vue
@@ -0,0 +1,73 @@
+<template>
+    <div>
+        <header class="header">
+            <div class="search-container">
+                <input type="text" class="search-input">
+            </div>
+        </header>
+        <div class="tab-container">
+            <div class="tab-item"></div>
+            <div class="tab-item"></div>
+            <div class="swiper-container">
+                <div class="swiper-wrapper">
+                    <div class="swiper-slide">Slide 1</div>
+                    <div class="swiper-slide">Slide 2</div>
+                    <div class="swiper-slide">Slide 3</div>
+                </div>
+            </div>
+        </div>
+        <section class="section"></section>
+    </div>
+</template>
+
+<script>
+    import Swiper from 'swiper';
+    import 'swiper/dist/css/swiper.min.css';
+
+    export default {
+        mounted() {
+            const swiper = new Swiper('swiper-container');
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+    @import '../../../css/function.scss';
+
+    .header {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        height: setRem(86);
+        background-color: #d73e34;
+
+        .search-container {
+            .search-input {
+                width: setRem(535);
+                height: setRem(60);
+                border-radius: setRem(30);
+                background-color: #e0655d;
+            }
+        }
+    }
+
+    .tab-container {
+        position: relative;
+        height: setRem(302);
+        background-color: #dd4439;
+
+        .swiper-container {
+            position: absolute;
+            top: setRem(92);
+            left: 0;
+            right: 0;
+            margin: 0 auto;
+
+            .swiper-slide {
+                width: setRem(726);
+                height: setRem(282);
+                background-color: yellow;
+            }
+        }
+    }
+</style>
