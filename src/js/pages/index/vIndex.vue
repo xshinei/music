350b476ed1e5a93cf4dbf70a001dcf21eb6b5b24
zhangan <zhangan@gaodun.com>
2018-10-24 15:58:01 +0800

    完成轮播图

--- src/js/pages/index/vIndex.vue
+++ src/js/pages/index/vIndex.vue
@@ -8,17 +8,12 @@
         <div class="tab-container">
             <div class="tab-item"></div>
             <div class="tab-item"></div>
-            <div class="banner-container">
+            <div class="banner-container" v-if="banners.length">
                 <swiper :options="swiperOption" style="width: 363px;">
-                    <swiper-slide class="banner-item" style="background: green;">
-                        111
+                    <swiper-slide v-for="(item, index) in banners" :key="index" class="banner-item">
+                        <img :src="item.picUrl" width="100%" height="100%" alt="">
                     </swiper-slide>
-                    <swiper-slide class="banner-item">
-                        222
-                    </swiper-slide>
-                    <swiper-slide class="banner-item" style="background: blue;">
-                        333
-                    </swiper-slide>
+                    <div class="banner-pagination" slot="pagination"></div>
                 </swiper>
             </div>
         </div>
@@ -28,18 +23,39 @@
 
 <script>
     import { swiper, swiperSlide } from 'vue-awesome-swiper';
+    import { getBanner } from '../../api/index.js';
 
     export default {
         data() {
             return {
                 swiperOption: {
+                    loop: true,
                     autoplay: true,
                     setWrapperSize :true,
-                }
+                    speed: 500,
+                    pagination: {
+                        el: '.banner-pagination',
+                        bulletClass: 'banner-pagination-bullet',
+                        bulletActiveClass: 'banner-pagination-bullet-active'
+                    }
+                },
+                banners: []
             };
+        },
+        created() {
+            this._getBanner();
         },
         mounted() {
             
+        },
+        methods: {
+            _getBanner() {
+                getBanner().then(res => {
+                    if (res.code === 200) {
+                        this.banners = res.banners;
+                    }
+                });
+            }
         },
         components: {
             swiper,
@@ -86,6 +102,14 @@
                 width: setRem(726);
                 height: setRem(282);
                 background-color: yellow;
+            }
+
+            .banner-pagination {
+                position: absolute;
+                z-index: 10;
+                bottom: 0;
+                left: 0;
+                text-align: center;
             }
         }
     }
