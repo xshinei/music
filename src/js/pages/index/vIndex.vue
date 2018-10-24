dc8454bba6c5a6c101fcb139065e44d7425df580
zhangan <zhangan@gaodun.com>
2018-10-24 22:30:57 +0800

    tab item

--- src/js/pages/index/vIndex.vue
+++ src/js/pages/index/vIndex.vue
@@ -6,10 +6,11 @@
             </div>
         </header>
         <div class="tab-container">
-            <div class="tab-item"></div>
-            <div class="tab-item"></div>
+            <div class="tab-item active">个性推荐</div>
+            <div class="tab-item">主播电台</div>
+            <!-- <span class="underline"></span> -->
             <div class="banner-container" v-if="banners.length">
-                <swiper :options="swiperOption" style="width: 363px;">
+                <swiper :options="swiperOption" :style="`width: ${px2Rem(363)}; border-radius: 4px;`">
                     <swiper-slide v-for="(item, index) in banners" :key="index" class="banner-item">
                         <img :src="item.picUrl" width="100%" height="100%" alt="">
                     </swiper-slide>
@@ -85,9 +86,29 @@
     }
 
     .tab-container {
+        display: flex;
+        justify-content: center;
         position: relative;
         height: setRem(302);
         background-color: #dd4439;
+
+        .tab-item {
+            margin: setRem(22) setRem(89);
+            color: #fff;
+
+            &.active {
+                font-weight: 500;
+            }
+        }
+
+        .underline {
+            position: absolute;
+            top: setRem(62);
+            left: setRem(195);
+            width: setRem(62);
+            height: setRem(6);
+            background-color: #fff;
+        }
 
         .banner-container {
             position: absolute;
--- src/js/utils/init.js
+++ src/js/utils/init.js
@@ -1,6 +1,11 @@
+export function baseFontSize() {
+    const clientWidth = document.documentElement.clientWidth;
+    return clientWidth / 7.5;
+}
+
 function setFontSize() {
     const clientWidth = document.documentElement.clientWidth;
-    document.documentElement.style.fontSize = clientWidth / 7.5 + 'px';
+    document.documentElement.style.fontSize = baseFontSize() + 'px';
 }
 
 document.addEventListener('DOMContentLoaded', setFontSize);
